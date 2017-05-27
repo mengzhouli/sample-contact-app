d2bbb738dc6a7139bd901cbe811c4a3fdaa842f6
Mengzhou Li <mengzhou.li@gmail.com>
2017-05-27 14:13:10 -0400

    Added tag and tag tests -added tag string to array and back in contact model -updated test with tag manipulation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,18 @@
 class Contact < ApplicationRecord
   belongs_to :user
+
+  def get_tags
+    tag_list.split(',')
+  end
+
+  def set_tags(tag_arr)
+    self.update!(tag_list: tag_arr.join(','))
+  end
+
+  def add_tag(new_tag)
+    tag_arr = get_tags
+    tag_arr << new_tag
+    set_tags(tag_arr)
+  end
+
 end
--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,13 +1,26 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-   test "contact name" do
-     contact = contacts(:james)
-     assert contact.first_name == 'James'
-   end
+  test "contact name" do
+    contact = contacts(:james)
+    assert contact.first_name == 'James'
+  end
 
-   test "contact belongs to user" do
-   	contact = contacts(:james)
-   	assert contact.user.email =="john@doe.com"
-   end
+  test "contact belongs to user" do
+    contact = contacts(:james)
+    assert contact.user.email =="john@doe.com"
+  end
+
+  test "tag array from comma seperated string" do
+    contact = contacts(:james)
+    assert contact.tag_list == "competitor,family"
+    assert contact.get_tags == ["competitor", "family"]
+  end
+
+  test "add new tag to tag_list" do
+    contact = contacts(:james)
+    new_tag = "estranged"
+    contact.add_tag(new_tag)
+    assert contact.tag_list == "competitor,family,estranged"
+  end
 end
